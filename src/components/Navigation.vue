cd8fa1461083eeadff39854f372fba7a5024c3e3
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-02-10 19:18:16 -0800

    Navbar built, disabled mobile devices.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+<header class="flex">
+    <div class ="branding flex">
+        <img src = "@/assets/file-invoice-dollar-solid.png" alt="" />
+    </div>
+</header>
+</template>
+
+<script>
+export default {
+    name: "navigation-bar",
+};
+</script>
+<style scoped>
+    header {
+        z-index: 99;
+        background-color: #1e2139;
+        @media only screen and (min-width: 900px) {
+            min-height: 100%;
+            min-width: 90px;
+            flex-direction: column;
+            border-radius: 0 20px 20px 0;
+        }
+
+        .branding {
+            border-radius: 0 20px 20px 0;
+            background-color: #7c5dfa;
+            justify-content: center;
+            padding: 24px;
+            flex-direction: column;
+            @media only screen and (min-width: 900px) {
+                width: 100%;
+            }
+
+            img {
+                width: auto;
+                height: 30px;
+            }
+        }
+    }
+</style>
+
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home-container">
+        <div class="header flex"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Home-container",
+    components: {},
+    
+};
+</script>
+<style lang="">
+    
+</style>
